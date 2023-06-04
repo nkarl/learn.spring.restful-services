86720f24cf795e277809867d62037d3823e48f48
nkarl <21993921+nkarl@users.noreply.github.com>
2023-06-04 10:15:12 -0700

    Added ResponseEntity to send 201 on creating new user record.

--- src/main/java/learn/spring/RESTfulWebServices/user/UserDAOService.java
+++ src/main/java/learn/spring/RESTfulWebServices/user/UserDAOService.java
@@ -1,7 +1,10 @@
 package learn.spring.RESTfulWebServices.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,10 +25,16 @@ public class UserDAOService {
         return service.findOne(id);
     }
 
-//    @RequestMapping(path = "/users", method = RequestMethod.POST)
+    //    @RequestMapping(path = "/users", method = RequestMethod.POST)
     @PostMapping(path = "/users")
-    public void createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@RequestBody User user) {
         service.save(user);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(user.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 
 }
