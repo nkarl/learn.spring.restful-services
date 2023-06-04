65768abceeb7a7251bb49f74b37fa11116cda745
nkarl <21993921+nkarl@users.noreply.github.com>
2023-06-03 22:57:26 -0700

    Added a GetMapping for path variable.

--- src/main/java/learn/spring/RESTfulWebServices/HelloWorld/HelloWorldBean.java
+++ src/main/java/learn/spring/RESTfulWebServices/HelloWorld/HelloWorldBean.java
@@ -5,7 +5,7 @@ public class HelloWorldBean {
     private String message;
 
     public HelloWorldBean(String message) {
-        this.message = message + " Bean!";
+        this.message = message;
     }
 
     public String getMessage() {
--- src/main/java/learn/spring/RESTfulWebServices/HelloWorld/HelloWorldController.java
+++ src/main/java/learn/spring/RESTfulWebServices/HelloWorld/HelloWorldController.java
@@ -1,9 +1,6 @@
 package learn.spring.RESTfulWebServices.HelloWorld;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 // REST API
 @RestController
@@ -19,5 +16,12 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(
+                String.format("Hello World, %s!", name)
+        );
     }
 }
