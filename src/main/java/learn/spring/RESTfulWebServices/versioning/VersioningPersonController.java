da0b7e9e73774b4a3eda19c4f515f39e0c5a7b11
nkarl <21993921+nkarl@users.noreply.github.com>
2023-06-04 17:26:41 -0700

    Added Accept-Header/Media-Type/Content-Negotiation versioning.

--- src/main/java/learn/spring/RESTfulWebServices/versioning/VersioningPersonController.java
+++ src/main/java/learn/spring/RESTfulWebServices/versioning/VersioningPersonController.java
@@ -21,18 +21,28 @@ public class VersioningPersonController {
         return new PersonV1("Bob Charlie");
     }
 
-    @GetMapping("/v2/person")
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 getAcceptHeaderPersonV1() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping("/person")
     public PersonV2 getPersonV2() {
         return new PersonV2(new Name("Bob", "Charlie"));
     }
 
-    @GetMapping(path = "/v2/person", params = "version=2")
+    @GetMapping(path = "/person", params = "version=2")
     public PersonV2 getRequestParamPersonV2() {
         return new PersonV2(new Name("Bob", "Charlie"));
     }
 
     @GetMapping(path = "/person/header", params = "X-API-VERSION=1")
     public PersonV2 getRequestHeaderPersonV2() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 getAcceptHeaderPersonV2() {
         return new PersonV2(new Name("Bob", "Charlie"));
     }
 }
