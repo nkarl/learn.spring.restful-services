b61f0d7e7822bb987364a3395fce9f4ec953633f
nkarl <21993921+nkarl@users.noreply.github.com>
2023-06-17 21:57:45 -0700

    Added lombok to project.

--- src/main/java/learn/spring/RESTfulWebServices/p01_DAOModel/User.java
+++ src/main/java/learn/spring/RESTfulWebServices/p01_DAOModel/User.java
@@ -1,62 +1,29 @@
 package learn.spring.RESTfulWebServices.p01_DAOModel;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
-@Entity(name ="user_details")
+
+@Entity(name = "user_details")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @Size(min = 2, message = "Name must contain at least 2 characters.")
     private String name;
     @Past(message = "Birth Date should be in the past.")
     private LocalDate birthDate;
 
-    protected User () { }
-
-    public User(Integer id, String name, LocalDate birthDate) {
-        this.id = id;
-        this.name = name;
-        this.birthDate = birthDate;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", birthDate=" + birthDate +
-                '}';
-    }
 }
