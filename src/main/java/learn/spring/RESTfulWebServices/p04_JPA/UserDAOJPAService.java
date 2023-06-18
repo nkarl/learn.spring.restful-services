7492576891fc06c11b1f40427ab682525df26f46
nkarl <21993921+nkarl@users.noreply.github.com>
2023-06-17 21:35:07 -0700

    Updated.

--- src/main/java/learn/spring/RESTfulWebServices/p04_JPA/UserDAOJPAService.java
+++ src/main/java/learn/spring/RESTfulWebServices/p04_JPA/UserDAOJPAService.java
@@ -2,7 +2,6 @@ package learn.spring.RESTfulWebServices.p04_JPA;
 
 import jakarta.validation.Valid;
 import learn.spring.RESTfulWebServices.p01_DAOModel.User;
-import learn.spring.RESTfulWebServices.p01_DAOModel.UserDAO;
 import learn.spring.RESTfulWebServices.p02_ExceptionHandling.UserNotFoundException;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.ResponseEntity;
@@ -23,12 +22,12 @@ public class UserDAOJPAService {
     }
 
     @GetMapping("/jpa/users")
-    public List<User> retrieveAllUsers() {
+    public List<User> getAllUsers() {
         return repository.findAll();
     }
 
     @GetMapping("/jpa/users/{id}")
-    public EntityModel<Optional<User>> retrieveUser(@PathVariable int id) throws UserNotFoundException {
+    public EntityModel<Optional<User>> getUser(@PathVariable int id) throws UserNotFoundException {
         Optional<User> user = repository.findById(id);
         if (user.isEmpty())
             throw new UserNotFoundException("userId=" + id);
@@ -36,13 +35,12 @@ public class UserDAOJPAService {
         return EntityModel.of(user);
     }
 
-
     @DeleteMapping("/jpa/users/{id}")
     public void deleteUser(@PathVariable int id) throws UserNotFoundException {
         repository.deleteById(id);
     }
 
-    //    @RequestMapping(path = "/jpa/users", method = RequestMethod.POST)
+    // @RequestMapping(path = "/jpa/users", method = RequestMethod.POST)
     @PostMapping(path = "/jpa/users")
     public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
         repository.save(user);
